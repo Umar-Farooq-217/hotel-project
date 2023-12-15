6843e2fcf7d583c3200248ae3c77f2e5b60334fb
Your Name <uf29664@gmail.com>
2023-12-15 21:05:25 +0500

    'hotel'

--- app/components/first-nav/First-Nav.js
+++ app/components/first-nav/First-Nav.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
+import '../../globals.css'
 export default function FirstNav() {
   return (
-    <div className='bg-[#425059] '>
-    <div>
-    <div>  <Image src="/phone.png" width={15} height={15}  />   <p>03486596201</p>                  </div>
-    <div>       <Image src="/Icon.png" width={15} height={15}  />   <p>uf29664@gmail.com</p>              </div>
+    <div className='bg-[#474747] flex justify-between text-white text-sm px-[60px] nav'>
+    <div className='flex py-1'>
+    <div className='flex justify-center items-center '>  <Image className='mr-1' src="/phone.png" width={15} height={0}  />   <p>03486596201</p>                  </div>
+    <div className='flex justify-center items-center ml-5 '>       <Image className='mr-1' src="/Icon.png" width={15} height={15}  />   <p>uf29664@gmail.com</p>              </div>
     </div>
-    <div>
-    <div><Image src="/twitter.png" width={25} height={25}  />  </div>
-    <div><Image src="/fb.png" width={10} height={8}  />  </div>
-    <div><Image src="/Group.png" width={15} height={15}  />  </div>
-    <div><Image src="/github.png" width={15} height={15}  />  </div>
+    <div className='flex justify-center items-center '>
+    <div className='flex justify-center pt-1 bg-[#5C5C5C] rounded-2xl w-5 mx-1  '><Image  src="/twitter.png" width={20} height={20}  />  </div>
+    <div className='flex justify-center  bg-[#5C5C5C] rounded-2xl w-5 mx-1 '><Image  src="/fb.png" width={7} height={7}  />  </div>
+    <div className='flex justify-center  bg-[#5C5C5C] rounded-2xl w-5 mx-1 '><Image  src="/github.png" width={12} height={12}  />  </div>
+    <div className='flex justify-center  bg-[#5C5C5C] rounded-2xl w-5  mx-1 '><Image  src="/Group.png" width={12} height={12}  />  </div>
+  
     </div>
     
     
---
+++ app/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from '../button/Button'
+import Image from 'next/image'
+export default function Navbar() {
+  return (
+    <div>
+    <div> 
+    <Image src="" width="" height="" />
+
+    </div>
+    <ul>
+    <li></li>
+    </ul>
+    <Button/>
+    </div>
+  )
+}
