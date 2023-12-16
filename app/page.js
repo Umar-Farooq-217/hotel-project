e579b8f821eefebd5d28a11707bb0e3d13a5f71f
Your Name <uf29664@gmail.com>
2023-12-17 00:14:18 +0500

    'hotel'

--- app/page.js
+++ app/page.js
@@ -2,19 +2,25 @@ import Image from 'next/image'
 import './globals.css'
 import FirstNav from './components/first-nav/First-Nav'
 import Navbar from './components/navbar/Navbar'
+import Button from './components/button/Button'
 
 export default function Home() {
   return (
   <div>
   <FirstNav/>
   <Navbar/>
-  <div className='first '>
+  <div className='first flex justify-center items-center '>
+  <div>
+  <h1 className='text-[96px] font-[300] font-[Charmonman] text-center'>Best Food for <br/> Your Taste</h1>
   
-  
-  
-  
-  
-  
+  <p className='text-center'>Discover delectable cuisine and unforgettable moments <br /> in our welcoming, culinary haven.</p>
+   
+  <div className='flex justify-center items-center mt-8'>
+  <div  className='mx-5'><Button title='Book A Table'/></div>
+  <div  className='mx-5'><Button title='Explore Menu'/></div>
+ 
+  </div>
+  </div>
   </div>
   </div>
   )
