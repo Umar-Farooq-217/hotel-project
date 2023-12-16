0ebf173188a0e42ee186946ef8657adff8fb4b67
Your Name <uf29664@gmail.com>
2023-12-16 19:49:15 +0500

    'hotel'

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import FirstNav from './components/first-nav/First-Nav'
-import Navbar from './components/navbar/Navbar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <FirstNav/>
-    <Navbar/>
+   
+   
       <body className={inter.className}>{children}</body>
     </html>
   )
