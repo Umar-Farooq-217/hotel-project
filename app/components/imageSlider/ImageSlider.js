03c25ca940c37d557ac5a2f860c6d7b857aa859c
Your Name <uf29664@gmail.com>
2023-12-26 23:23:59 +0500

    hotel

---
+++ app/components/imageSlider/ImageSlider.js
@@ -0,0 +1,34 @@
+'use client'
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+class ImageSlider extends Component {
+  render() {
+    const { images } = this.props;
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    };
+
+    return (
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Slide ${index}`} />
+          </div>
+        ))}
+      </Slider>
+    );
+  }
+}
+
+export default ImageSlider;
--- app/menu/page.js
+++ app/menu/page.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import ImageSlider from '../components/imageSlider/ImageSlider';
+
+const images = [
+    '/burger.png',
+    '/chips.png',
+    '/mustafa.jpeg',
+    // Add more image paths as needed
+  ];
 
 export default function page() {
+  
+      
   return (
-    <div>page</div>
+    <div>
+    <div>
+    <h1>Your Image Slider</h1>
+    <ImageSlider images={images} />
+  </div>
+    
+    </div>
   )
 }
